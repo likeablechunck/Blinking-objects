0b7e002ba93d959f7d84dd9328bda43d75ef356c
Faranak Razavi <FRAZAVI2@art.edu>
2016-09-15 13:51:58 -0700

    trying different Blink scripts to work

---
+++ Blinking_Test/Assets/Blink.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Blink : MonoBehaviour
+{
+
+    public GameObject ball; //assign the game object sphere or what ever you want to blink
+    public double timer;
+    public bool onoff;
+    void Update()
+    {
+        sphere_blink();
+    }
+
+    void sphere_blink()
+    {
+        if (Time.time > timer)
+        {
+
+            timer = Time.time + .4;
+            onoff = !onoff;
+            //ball.renderer.enabled = onoff;
+            ball.GetComponent<Renderer>().enabled = onoff;
+
+        }
+    }
+}
---
+++ Blinking_Test/Assets/Game_Blinking.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Game_Blinking : MonoBehaviour
+{
+    public GameObject sphere1;
+    public GameObject sphere2;
+    public GameObject cube;
+    float timer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timer = Time.time;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        print("Timer is at :"+timer);
+
+	
+	}
+    public void renderOn(GameObject object)
+    {
+
+    }
+}
